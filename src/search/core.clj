9425ee614cd4c449001284206285a37527be368c
Denis Laliberté <denis@tp1.ca>
2014-12-31 12:26:21 -0500

    remove hardcoded middle index

--- src/search/core.clj
+++ src/search/core.clj
@@ -27,7 +27,7 @@
     (= a (get b (middleIndex b))) (middleIndex b)
     (moreThanOne b)
       (cond
-        (greaterThanMiddle a b) (cond (= -1 (search a (secondHalf b)) ) -1 :else (+ 2 (search a (secondHalf b))))
+        (greaterThanMiddle a b) (cond (= -1 (search a (secondHalf b)) ) -1 :else (+ (inc (middleIndex b)) (search a (secondHalf b))))
         :else (search a (firstHalf b))
       )
     :else -1
